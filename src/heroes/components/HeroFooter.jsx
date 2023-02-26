7d0ebc9cf825d3336578e3a4e9e7139ed745ee26
M0n00s <programacion.buffalo@gmail.com>
2023-02-26 14:59:06 -0400

    Change/ footer description

--- src/heroes/components/HeroFooter.jsx
+++ src/heroes/components/HeroFooter.jsx
@@ -5,13 +5,16 @@ export const HeroFooter = () => {
     <>
       <div className="mt-5 card text-center bg-dark " data-bs-theme="dark">
         <div className="card-body text-white p-5">
-          <h5 className="card-title">Special title treatment</h5>
+          <h5 className="card-title">developed by abdel arocha</h5>
           <p className="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
+            React course development by Fernando Herrera
           </p>
-          <a href="#" className="btn btn-primary">
-            Go somewhere
+          <a
+            href="https://www.linkedin.com/in/abdel-arocha-732507b2/"
+            className="btn btn-primary"
+            target="_blank"
+          >
+            Linkedin
           </a>
         </div>
       </div>
